47d2174325d02a2244ce54f94baebe5906906a29
Ekaterina <e406096@gmail.com>
2026-09-07 10:12:37 +0200

    Add protected frontend routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,45 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import DashboardPage from './pages/DashboardPage/DashboardPage'
-import ClientsPage from './pages/ClientsPage/ClientsPage'
-import CallsPage from './pages/CallsPage/CallsPage'
-import Navigation from './widgets/Navigation/Navigation'
-import LoginPage from './pages/LoginPage/LoginPage'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DashboardPage from "./pages/DashboardPage/DashboardPage";
+import ClientsPage from "./pages/ClientsPage/ClientsPage";
+import CallsPage from "./pages/CallsPage/CallsPage";
+import Navigation from "./widgets/Navigation/Navigation";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import ProtectedRoute from "./app/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
-     <Navigation />
+      <Navigation />
       <Routes>
-        <Route path="/" element={<DashboardPage />} />
-        <Route path="/clients" element={<ClientsPage />} />
-        <Route path="/calls" element={<CallsPage />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/clients"
+          element={
+            <ProtectedRoute>
+              <ClientsPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/calls"
+          element={
+            <ProtectedRoute>
+              <CallsPage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
